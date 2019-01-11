e31be00c6f2949d6b8100a34ac208027c4051107
Peter Ringelmann <peter.ringelmann@epost-dev.de>
2019-01-11 15:16:55 +0100

    Update scala problem 1

--- problems/1/scala/1.scala
+++ problems/1/scala/1.scala
@@ -2,6 +2,8 @@ import scala.math._
 
 object HelloWorld {
 
+    val n = 1000
+
     // Simple solution using modulo arithmetic
     def sumMultipleModulo(number: Int, factors: List[Int]): Int = {
         var sum = 0
@@ -14,19 +16,21 @@ object HelloWorld {
         sum
     }
 
-    // Simple solution using modulo arithmetic
+    // Using formulae for arithmetic progressions
     def sumMultipleFormula(number: Int, factors: List[Int]): Int = {
-        val sumFactor = (f:Int):Int => {
-            val s = loor(number / f)
-            val l = f + ((s - 1) * f)
-            val sum = s * (f + l) / 2
+        def sumFactor(f: Int):Int = { 
+            var s = floor(number / f)
+            var l = f + ((s - 1) * f)
+            var sum = s * (f + l) / 2
+            sum.toInt
         }
-        sumFactor(1)
+        factors.map(sumFactor).reduce(_ + _) - sumFactor(factors.reduce(_ * _))
     }
 
-    // Using formulae for arithmetic progressions
     def main(args: Array[String]): Unit = {
-        val sum = sumMultipleModulo(10, List(3,5))
+        val sum = sumMultipleModulo(n, List(3,5))
         println(sum)
+        val sum2 = sumMultipleFormula(n, List(3,5))
+        println(sum2)
     }
 }
